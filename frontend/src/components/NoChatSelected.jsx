985b8e6e804d9d68f372da7a04e921c7cc145eb7
Arman Khan <armankkhan14@gmail.com>
2025-07-11 14:44:44 +0530

    sixth commit

--- frontend/src/components/NoChatSelected.jsx
+++ frontend/src/components/NoChatSelected.jsx
@@ -15,7 +15,7 @@ const NoChatSelected = () => {
             </div>
 
             {/* Welcome Text */}
-            <h2 className='text-2xl font-bold'>Welcome To Baateinkro!</h2>
+            <h2 className='text-2xl font-bold'>Welcome To Chatkro!</h2>
             <p className='text-base-content/60'>
             Select a conversation from the sidebar to start chatting</p>
             
